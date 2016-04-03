7f7ae32f901d8414f06cc468688117a8814d82c4
sasakero <tanayasu2012@gmail.com>
2016-04-03 12:34:52 +0900

    初UP

---
+++ GlowBall/Assets/Scripts/FallInChecker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallInChecker : MonoBehaviour {
+
+    public GUISkin gsk;
+
+    public Hole red;
+    public Hole blue;
+    public Hole green;
+    
+    void OnGUI()
+    {
+        string label = " ";
+        if(red.IsFallIn() && blue.IsFallIn() && green.IsFallIn())
+        {
+            label = "All ball is Fall in hole!";
+        }
+        GUILayout.BeginArea(new Rect(0,0,500,48));
+        GUILayout.Label(label, gsk.label);
+        GUILayout.EndArea();
+    } 
+}
---
+++ GlowBall/Assets/Scripts/GravityContller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityContller : MonoBehaviour {
+
+    const float GRAVITY = 9.81f;
+
+    public float gravityScale = 1.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vector = new Vector3();
+
+        if (Application.isEditor){
+
+            vector.x = Input.GetAxis("Horizontal");
+            vector.z = Input.GetAxis("Vertical");
+
+            if (Input.GetKey("space"))
+            {
+                vector.y = 1.0f;
+            }
+            else
+            {
+                vector.y = -1.0f;
+            }
+
+            Physics.gravity = GRAVITY * vector.normalized * gravityScale;
+        }else{
+            //加速度センサ対応
+            vector.x = Input.acceleration.x;
+            vector.z = Input.acceleration.y;
+            vector.y = Input.acceleration.z;
+        }
+	}
+}
